434ffc24b5781185996b04198f09855983ca9dcc
Brian Stolarski <stolarbr@sheridancollege.ca>
2024-11-19 17:56:10 -0500

    new answers

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,8 +3,9 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         for (int i = 0; i < number; i++) {
-            System.out.println("*");
+            System.out.print("*");
         }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
@@ -14,14 +15,22 @@ public class AdvancedAstrology {
     }
 
     public static void printTriangle(int size) {
-        for (int i = 0; i < size; i++) {
-            printSpaces(size);
-            printStars(size);
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
         }
+
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
+        }
+        for (int i = 0; i < 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
