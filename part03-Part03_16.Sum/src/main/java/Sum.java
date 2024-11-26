cc576730adabcea00dd9c091afa7552096ea8c35
Brian Stolarski <stolarbr@sheridancollege.ca>
2024-11-25 23:37:29 -0500

    more ArrayLists
    need to practice

--- part03-Part03_16.Sum/src/main/java/Sum.java
+++ part03-Part03_16.Sum/src/main/java/Sum.java
@@ -3,8 +3,27 @@ import java.util.ArrayList;
 
 public class Sum {
 
-    public static void main(String[] args) {
-        // Try your method here
+    public static int sum(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum = number + sum;
+        }
+        return sum;
     }
 
+    public static void main(String[] args) {
+        // ArrayList<Integer> numbers = new ArrayList<>();
+
+        // numbers.add(3);
+        // numbers.add(3);
+        // numbers.add(3);
+        // numbers.add(-1);
+        // System.out.println(sum(numbers));
+
+        // numbers.add(5);
+        // numbers.add(1);
+        // System.out.println(sum(numbers));
+        // }
+
+    }
 }
--- part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -3,8 +3,28 @@ import java.util.ArrayList;
 
 public class RemoveLast {
 
+    public static void removeLast(ArrayList<String> strings) {
+        if (strings.size() == 0) {
+            return;
+        }
+        if (strings.size() > 0) {
+            int lastIndex = strings.size() - 1;
+            strings.remove(lastIndex);
+        }
+
+    }
+
     public static void main(String[] args) {
-        // Try your method in here
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Brian");
+        names.add("George");
+        names.add("Jerry");
+        names.add("Todd");
+
+        removeLast(names);
+
+        System.out.println(names);
     }
 
 }
