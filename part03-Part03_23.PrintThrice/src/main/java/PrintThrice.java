5e026d751e440c56335a119a12c1b021c4be24ed
Brian Stolarski <stolarbr@sheridancollege.ca>
2024-11-26 23:40:42 -0500

    strings n stuff

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
 
 public class PrintThrice {
 
+    // public static String Word() {
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.print("Give a word: ");
+    //     String thing = scanner.nextLine();
+    //     return thing;
+    // }
+
+    // public static void main(String[] args) {
+    //     String thing = Word();
+    //     System.out.println(thing + thing + thing);
+
+    // }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Give a word: ");
+        String thing = scanner.nextLine();
 
-        // Write your program here
-
+        System.out.println(thing + thing + thing);
     }
 }
+
